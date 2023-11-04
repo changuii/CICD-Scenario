ecd0a50ea33e49f81bf202ed88f902cdeaf10169
changuii <rhljh7410@gmail.com>
2023-11-04 21:47:27 +0900

    make endpoint

---
+++ src/main/java/jenkins/test/test/controller/TestController.java
@@ -0,0 +1,23 @@
+package jenkins.test.test.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+
+    @GetMapping()
+    public String readString(){
+        return "HELLO WORLD!!";
+    }
+
+    @GetMapping("/hi")
+    public String readhiString(){
+        return "hi";
+    }
+
+}
