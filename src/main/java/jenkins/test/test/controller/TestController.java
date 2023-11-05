6411b1e5c340ec23876f018f44781655cb32e72b
changuii <rhljh7410@gmail.com>
2023-11-05 21:16:33 +0900

    qwe

--- src/main/java/jenkins/test/test/controller/TestController.java
+++ src/main/java/jenkins/test/test/controller/TestController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
@@ -14,14 +15,14 @@ public class TestController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping()
-    public String readString(){
-        logger.info("readString 진행중입니다.");
+    public String readString(HttpServletRequest request){
+        logger.info("readString 진행중입니다. "+request.getRemoteAddr());
         return "HELLO WORLD!!";
     }
 
     @GetMapping("/hi")
-    public String readhiString(){
-        logger.info("read hi 진행중입니다.");
+    public String readhiString(HttpServletRequest request){
+        logger.info("read hi 진행중입니다. " + request.getRemoteAddr());
 
         return "hi";
     }
