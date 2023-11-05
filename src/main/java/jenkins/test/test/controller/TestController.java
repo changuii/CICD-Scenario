a78c7c31bc6c9b491e0967e7d66add19867be629
changuii <rhljh7410@gmail.com>
2023-11-05 19:02:34 +0900

    ✅ update

--- src/main/java/jenkins/test/test/controller/TestController.java
+++ src/main/java/jenkins/test/test/controller/TestController.java
@@ -1,23 +1,37 @@
 package jenkins.test.test.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping()
     public String readString(){
+        logger.info("readString 진행중입니다.");
         return "HELLO WORLD!!";
     }
 
     @GetMapping("/hi")
     public String readhiString(){
+        logger.info("read hi 진행중입니다.");
+
         return "hi";
+    }
+
+    @PostMapping("")
+    public Map<Object, Object> post(
+            @RequestBody Map<Object, Object> json
+            ){
+        logger.info("post 중입니다. :" + json);
+        return json;
     }
 
 }
