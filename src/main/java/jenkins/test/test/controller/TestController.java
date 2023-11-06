2fd75cd9e2a7922320e5991374c231cb80fea49c
changuii <rhljh7410@gmail.com>
2023-11-06 20:37:56 +0900

    update

--- src/main/java/jenkins/test/test/controller/TestController.java
+++ src/main/java/jenkins/test/test/controller/TestController.java
@@ -17,7 +17,7 @@ public class TestController {
     @GetMapping()
     public String readString(HttpServletRequest request){
         logger.info("readString 진행중입니다. "+request.getRemoteAddr());
-        return "HELLO WORLD!!!!";
+        return "HELLO WORLD!!!!HELLO WORLD!!!!HELLO WORLD!!!!HELLO WORLD!!!!HELLO WORLD!!!!HELLO WORLD!!!!";
     }
 
     @GetMapping("/hi")
