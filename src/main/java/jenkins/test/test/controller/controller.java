80d188c2bc7d7e0f924a531b9d03bd6aa4b3c910
changuii <rhljh7410@gmail.com>
2023-12-06 15:25:16 +0900

    update

--- src/main/java/jenkins/test/test/controller/controller.java
+++ src/main/java/jenkins/test/test/controller/controller.java
@@ -1,15 +1,22 @@
 package jenkins.test.test.controller;
 
 
+import java.net.http.HttpRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class controller {
-
+    private static final Logger logger = LoggerFactory.getLogger(controller.class);
 
     @GetMapping("/")
-    public String homePage(){
+    public String homePage(HttpServletRequest request){
+        logger.info(request.getRemoteAddr());
         return "home";
     }
 }
